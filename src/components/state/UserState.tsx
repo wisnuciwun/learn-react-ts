fb697c7ebdfc268b0200ed18f2c1fac0dbf74da9
wisnuciwun <adiwardhanawisnu@gmail.com>
2022-01-26 21:25:57 +0700

    learn part 9 - type assertion

--- src/components/state/UserState.tsx
+++ src/components/state/UserState.tsx
@@ -6,13 +6,16 @@ type UserStateProps = {
 }
 
 export const UserState = () => {
-    const [login, setlogin] = useState< UserStateProps | null >(null);
+    // const [login, setlogin] = useState< UserStateProps | null >(null);
+    const [login, setlogin] = useState< UserStateProps >({} as UserStateProps); // if you want to avoid {login?.name} when logout because its error if u use method above
+
 
     const handleLogin = () => { setlogin({
         name: "Wisnu",
         email: "wisnu@gmail.com"
     }) }
-    const handleLogout = () => { setlogin(null) }
+    // const handleLogout = () => { setlogin(null) }
+    const handleLogout = () => { setlogin({name: "", email: ""}) } // but it comes with this rule
 
     return (
     <div>
